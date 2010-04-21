7bfb05b1b5f837b3a69d36037878ee1355a44252
Joshua Hawxwell <joshhawxwell@googlemail.com>
2010-04-21 18:21:08 +0100

    Sorted out multiline comments and added the ability to generate just a single file

---
+++ Support/Scripts/create_single_css.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+require File.join( File.dirname(__FILE__), 'haml/lib/haml' )
+require File.join( File.dirname(__FILE__), 'haml/lib/haml/exec' )
+
+input = STDIN.read
+Dir.chdir(ENV['E_DIRECTORY'])
+
+files = "#{ENV['E_FILENAME']}:"
+files += "#{ENV['E_FILENAME'].gsub('.sass', '.css')}"
+
+arg = ['--update', files]
+o = Haml::Exec::Sass.new(arg)
+o.parse!
